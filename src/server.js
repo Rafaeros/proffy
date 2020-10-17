ab9b6ddd6af3a4de1e7c5920d97d93e247a5647b
Rafaeros <rafaeladriancordeirocosta@gmail.com>
2020-10-17 17:50:38 -0300

    Projeto adicionado

---
+++ src/server.js
@@ -0,0 +1,24 @@
+﻿const express = require("express")
+const server = express()
+
+const { pageLanding, pageStudy, pageGiveClasses, saveClasses } = require("./pages")
+
+//Configuração do nunjucks
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true,
+})
+//Servidor
+server
+.use(express.urlencoded({extended: true}))
+.use(express.static("public"))
+//Rotas da aplicação
+.get("/", pageLanding)
+.get("/study", pageStudy)
+.get("/give-classes", pageGiveClasses)
+.post("/save-classes", saveClasses)
+//Porta 5500
+.listen(5500, () => {
+    console.log('Server started')
+})
